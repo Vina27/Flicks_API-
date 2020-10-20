06a3bc450e2171d9fdb5acad648211099ccf66f5
Vina27 <Mtabali27@gmail.com>
2020-10-20 16:03:08 -0400

    added dependent and destroy to movie and review controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-    has_many :reviews 
+    has_many :reviews, dependent: :destroy
     has_many :users, through: :reviews 
     has_many :actors 
     belongs_to :genre
